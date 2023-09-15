39abc673fb08777c84d221d83817fb10fae2698d
Aditya Veer Parmar <nakulparmar15@gmail.com>
2023-09-15 17:34:11 +0530

    revert bg image stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import { astroImageTools } from "astro-imagetools";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), astroImageTools],
+  integrations: [react(), tailwind()],
 });
