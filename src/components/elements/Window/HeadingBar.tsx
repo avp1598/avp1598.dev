ebb3f3dfc29baee55875e74e92a6e8933fa88513
Aditya Veer Parmar <nakulparmar15@gmail.com>
2023-09-18 15:16:39 +0530

    feat: add expand screen

--- src/components/elements/Window/HeadingBar.tsx
+++ src/components/elements/Window/HeadingBar.tsx
@@ -1,19 +1,22 @@
-import React from "react";
 import styles from "./styles.module.css";
 import icon from "@/static/home.png";
 
 type Props = {
   title: string;
+  onExpandClick: () => void;
 };
 
-const HeadingBar = ({ title }: Props) => {
+const HeadingBar = ({ title, onExpandClick }: Props) => {
   return (
     <div className="heading-bar">
       <div className={styles.wrapper}>
         <div className="relative md:absolute flex flex-row gap-2 ml-3">
           <div className="bg-[rgb(255,95,88)] h-[0.9rem] w-[0.9rem] rounded-full border border-[hsl(2.5,100%,55.5%)] cursor-pointer" />
           <div className="bg-[rgb(255,189,46)] h-[0.9rem] w-[0.9rem] rounded-full border border-[hsl(41,100%,48.7%)] cursor-pointer" />
-          <div className="bg-[rgb(38,201,0)] h-[0.9rem] w-[0.9rem] rounded-full border border-[hsl(100,100%,40.8%)] cursor-pointer" />
+          <div
+            className="bg-[rgb(38,201,0)] h-[0.9rem] w-[0.9rem] rounded-full border border-[hsl(100,100%,40.8%)] cursor-pointer"
+            onClick={onExpandClick}
+          />
         </div>
         <div className="flex flex-row mx-2 gap-2 items-center justify-center w-full">
           <img
--- src/components/elements/Window/index.tsx
+++ src/components/elements/Window/index.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Draggable from "react-draggable";
 import HeadingBar from "./HeadingBar";
+import { animated, useSpring } from "@react-spring/web";
 
 type Props = {
   title: string;
@@ -8,22 +9,52 @@ type Props = {
 };
 
 const Window = ({ title, children }: Props) => {
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  const [pos, setPos] = useState({ x: 0, y: 0 });
+  const [oldPos, setOldPos] = useState({ x: 0, y: 0 });
+
+  const toggleFullScreen = () => {
+    if (!isFullScreen) {
+      setOldPos({ x: pos.x, y: pos.y });
+      setPos({ x: 0, y: 0 });
+    } else {
+      setPos({ x: oldPos.x, y: oldPos.y });
+    }
+    setIsFullScreen(!isFullScreen);
+  };
+
+  const styles = useSpring({
+    to: {
+      height: isFullScreen ? "91.5%" : "50%",
+      minWidth: isFullScreen ? "100%" : "60%",
+      minHeight: isFullScreen ? "91.5%" : "25%",
+      maxWidth: isFullScreen ? "100%" : "80%",
+    },
+    config: {
+      mass: 1,
+      tension: 300,
+      friction: 30,
+    },
+  });
+
   return (
-    <div className="grid place-items-center min-h-screen overflow-auto">
-      <Draggable bounds="parent" handle=".heading-bar">
-        <div
-          className="rounded-lg shadow-xl resize overflow-hidden backdrop-blur-lg bg-[rgb(23,23,23,0.85)]"
-          style={{
-            height: "50%",
-            minWidth: "60%",
-            minHeight: "25%",
-            maxHeight: "80%",
-            maxWidth: "80%",
-          }}
+    <div className="grid place-items-center min-h-screen overflow-hidden">
+      <Draggable
+        handle=".heading-bar"
+        position={pos}
+        onDrag={(e, data) => {
+          setPos({ x: data.x, y: data.y });
+        }}
+      >
+        <animated.div
+          className={`rounded-lg shadow-xl resize overflow-hidden backdrop-blur-lg bg-[rgb(23,23,23,0.85)] ${
+            isFullScreen ? "mt-[-35px]" : ""
+          }`}
+          style={styles}
         >
-          <HeadingBar title={title} />
+          <HeadingBar title={title} onExpandClick={toggleFullScreen} />
           {children}
-        </div>
+        </animated.div>
       </Draggable>
     </div>
   );
